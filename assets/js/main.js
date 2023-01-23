c8fc7855ef8b3be3adf3135807b4b5042aa65505
Mitch <mitchlovo10@gmail.com>
2023-01-23 13:50:00 -0600

    avances en estructuracion del proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,65 @@
 import { getItems } from "./firestore.js";
 
-const buton = document.getElementsByClassName('boton')
-let categorias = document.querySelector('.categorias')
-let todos
+window.addEventListener("DOMContentLoaded", function(event) {
+
+  
+  let categorias = document.querySelector('.categorias')
+  let todos
+
+  /* hover.addEventListener ("mousehover", function(e){
+    console.log('hover')
+  })
+ */
 
 
-buton.addEventListener("click", (e) => {
-  console.log('click')
-})
 
-async function loadTodos() {
-  todos = []
+  async function loadTodos() {
+    todos = []
 
-  try {
-    const response = await getItems();
+    try {
+      const response = await getItems();
 
-    todos = [...response];
-    mostrar()
-  } catch (error) {
-    console.error(error);
+      todos = [...response];
+      mostrar()
+    } catch (error) {
+      console.error(error);
+    }
+
+    return todos
   }
 
-  return todos
-}
+  const mostrar = ()=>{
+    let cat = ""
+    todos.forEach(element => {
+      console.log(element)
+      cat += `
+      <div class="categoria">
+        <img src="${element.img}" alt="" />
+        <h3>${element.nombre}</h3>
+        <p>${element.descripcion}</p>
+      </div>
+      ` 
+    });
+    categorias.innerHTML = cat
+    console.log(cat)
+  }
 
-const mostrar = ()=>{
-  let cat = ""
-  todos.forEach(element => {
-    console.log(element)
-    cat += `
-    <div class="categoria">
-      <img src="${element.img}" alt="" />
-      <h3>${element.nombre}</h3>
-      <p>${element.descripcion}</p>
-    </div>
-    ` 
+
+  window.addEventListener("scroll", function () {
+    let menu = document.querySelector(".menu");
+    menu.classList.toggle("scroll", this.window.scrollY > 0);
+
+    //console.log(menu);
   });
-  categorias.innerHTML = cat
-  console.log(cat)
-}
 
 
-window.addEventListener("scroll", function () {
-  let menu = document.querySelector(".menu");
-  menu.classList.toggle("scroll", this.window.scrollY > 0);
 
-  //console.log(menu);
-});
 
-window.addEventListener("load", function(){
-  console.log("loadd")
-  //loadTodos()
+
+  window.addEventListener('load', (e)=>{
+  
+  })
+
+
+
 })
-
-
